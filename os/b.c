c718ea7adf8c699d34f3dd8a28a6305f5cc846e6
Tushar Sonar <107312969+the-tushar-sonar@users.noreply.github.com>
2025-10-01 06:09:22 +0000

    Refactor process creation example in b.c to improve readability and structure

--- os/b.c
+++ os/b.c
@@ -3,29 +3,53 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 
-int main() {
-    pid_t parent = getpid();    // store parent PID
-    pid_t pid = fork();         // create child process
+int main()
+{
+    pid_t parent = getpid(); // store parent PID
+    pid_t pid = fork();      // create child process
 
-    if (pid == -1) {
+    if (pid == -1)
+    {
         // fork failed
         perror("fork failed");
         exit(EXIT_FAILURE);
-    } 
-    else if (pid > 0) {
+    }
+    else if (pid > 0)
+    {
         // Parent process
-        int status;
-        printf("Parent (PID: %d) waiting for child...\n", parent);
-        printf("Status: %d",status);
-        waitpid(pid, &status, 0); // wait for child to finish
+        printf("Parent (PID: %d) is running...\n",parent);
+
+        // Getting input
+        int n=5,i,j,temp;
+        int arr[n];
+
+        printf("Enter the 5 numbers");
+        for(i=0;i<n;i++)
+        {
+            scanf("")
+
+        }
+
+
+
+
+
+
+
+      
+        printf("waiting for child...\n");
         
+        int status;
+        waitpid(pid, &status, 0); // wait for child to finish
+
         printf("Parent (PID: %d) detected child finished.\n", parent);
-    } 
-    else {
+    }
+    else
+    {
         // Child process
         printf("Child (PID: %d) running...\n", getpid());
         // Instead of execve, just print message
-        _exit(EXIT_SUCCESS);      // exit child
+        _exit(EXIT_SUCCESS); // exit child
     }
 
     return 0;
