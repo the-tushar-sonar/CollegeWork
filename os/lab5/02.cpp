901bd303964291281494f07b03a23162c91e9558
Tushar Sonar <107312969+the-tushar-sonar@users.noreply.github.com>
2025-10-15 06:00:00 +0000

    Refactor FCFS scheduling implementation to use a Process struct and improve code clarity

--- os/lab5/02.cpp
+++ os/lab5/02.cpp
@@ -1,62 +1,94 @@
 #include <iostream>
 #include <iomanip>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int main() {
+// Define process structure
+struct Process
+{
+    int id;
+    int burst;
+    int arrival;
+    int completion;
+    int waiting;
+    int turnaround;
+    int response;
+    float penalty;
+};
+
+int main()
+{
     int n;
     cout << "Enter the number of processes -- ";
     cin >> n;
 
-    int burst[n], waiting[n], turnaround[n], response[n];
-    float penalty[n];
+    vector<Process> p(n);
 
-    // Input burst times
-    for (int i = 0; i < n; i++) {
-        cout << "Enter Burst Time for Process " << i << " -- ";
-        cin >> burst[i];
+    // Input arrival and burst times
+    for (int i = 0; i < n; i++)
+    {
+        p[i].id = i;
+        cout << "Enter Arrival Time & Burst Time for Process " << i << " -- ";
+        cin >> p[i].arrival;
+        cin >> p[i].burst;
     }
 
-    // FCFS calculations
-    waiting[0] = 0; // first process has no waiting time
-    for (int i = 1; i < n; i++)
-        waiting[i] = waiting[i - 1] + burst[i - 1];
+    // Sort processes by arrival time
+    sort(p.begin(), p.end(), [](const Process &a, const Process &b)
+         { return a.arrival < b.arrival; });
 
-    // Turnaround, response, and penalty ratio
-    for (int i = 0; i < n; i++) {
-        turnaround[i] = waiting[i] + burst[i];
-        response[i] = waiting[i]; // same for FCFS
-        penalty[i] = (float)turnaround[i] / burst[i];
+    // FCFS scheduling calculations
+    int current_time = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (current_time < p[i].arrival)
+            current_time = p[i].arrival; 
+        p[i].waiting = current_time - p[i].arrival;
+        p[i].response = p[i].waiting;
+        p[i].completion = current_time + p[i].burst; 
+        p[i].turnaround = p[i].completion - p[i].arrival;
+        p[i].penalty = (float)p[i].turnaround / p[i].burst;
+
+        current_time += p[i].burst;
     }
 
     // Compute averages
-    float avg_wt = 0, avg_tat = 0, avg_rt = 0, avg_pr = 0;
-    for (int i = 0; i < n; i++) {
-        avg_wt += waiting[i];
-        avg_tat += turnaround[i];
-        avg_rt += response[i];
-        avg_pr += penalty[i];
+    float total_wt = 0, total_tat = 0, total_rt = 0, total_pr = 0;
+    for (auto &proc : p)
+    {
+        total_wt += proc.waiting;
+        total_tat += proc.turnaround;
+        total_rt += proc.response;
+        total_pr += proc.penalty;
     }
 
-    avg_wt /= n;
-    avg_tat /= n;
-    avg_rt /= n;
-    avg_pr /= n;
+    float avg_wt = total_wt / n;
+    float avg_tat = total_tat / n;
+    float avg_rt = total_rt / n;
+    float avg_pr = total_pr / n;
 
     // Throughput = processes completed / total time
-    float total_time = turnaround[n - 1];
-    float throughput = (float)n / total_time;
+    float total_time = 0;
+    for (auto &proc : p)
+        total_time = max(total_time, (float)proc.completion);
+    float throughput = n / total_time;
 
-    // Display output
-    cout << "\nPROCESS\tBURST TIME\tWAITING TIME\tTURNAROUND TIME\tRESPONSE TIME\tPENALTY RATIO\n";
-    for (int i = 0; i < n; i++) {
-        cout << "P" << i << "\t" << burst[i] << "\t\t"
-             << waiting[i] << "\t\t"
-             << turnaround[i] << "\t\t"
-             << response[i] << "\t\t"
-             << fixed << setprecision(2) << penalty[i] << endl;
+    // Display results
+    cout << "\nPROCESS\tARRIVAL TIME\tBURST TIME\tCOMPLETION TIME\tWAITING TIME\tTURNAROUND TIME\tRESPONSE TIME\tPENALTY RATIO\n";
+    for (auto &proc : p)
+    {
+        cout << "P" << proc.id << "\t"
+             << proc.arrival << "\t\t"
+             << proc.burst << "\t\t"
+             << proc.completion << "\t\t"
+             << proc.turnaround << "\t\t"
+             << proc.waiting << "\t\t"
+             << proc.response << "\t\t"
+             << fixed << setprecision(2) << proc.penalty << endl;
     }
 
-    cout << "Average Waiting Time-- " << fixed << setprecision(6) << avg_wt << endl;
+    cout << "\nAverage Waiting Time-- " << fixed << setprecision(6) << avg_wt << endl;
     cout << "Average Turnaround Time -- " << avg_tat << endl;
     cout << "Average Response Time -- " << avg_rt << endl;
     cout << "Average Penalty Ratio -- " << avg_pr << endl;
