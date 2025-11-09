207f834540051af4ae1e2c29b6c71871dac7cf69
Tushar Sonar <107312969+the-tushar-sonar@users.noreply.github.com>
2025-11-09 21:48:58 +0530

    Implement the the algorithm for the shortest job first (non preemptive)

---
+++ os/sjfNp.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct Process {
+    int id, at, bt;
+    int ct = 0, tat = 0, wt = 0, rt = 0;
+    float pr = 0.0;
+    bool completed = false;
+};
+
+int main() {
+    int n;
+    cout << "Enter number of processes: ";
+    cin >> n;
+
+    vector<Process> p(n);
+    for (int i = 0; i < n; i++) {
+        cout << "Enter Arrival Time & Burst Time for Process " << i << ": ";
+        cin >> p[i].at >> p[i].bt;
+        p[i].id = i;
+    }
+
+    int completed = 0, current_time = 0;
+    string gantt = "";
+
+    while (completed < n) {
+        int idx = -1;
+        int min_bt = 1e9;
+
+        for (int i = 0; i < n; i++) {
+            if (!p[i].completed && p[i].at <= current_time) {
+                if (p[i].bt < min_bt) {
+                    min_bt = p[i].bt;
+                    idx = i;
+                }
+            }
+        }
+
+        if (idx == -1) {
+            current_time++;
+            continue;
+        }
+
+        // Start execution
+        p[idx].rt = current_time - p[idx].at;
+        current_time += p[idx].bt;
+        p[idx].ct = current_time;
+        p[idx].tat = p[idx].ct - p[idx].at;
+        p[idx].wt = p[idx].tat - p[idx].bt;
+        p[idx].pr = (float)p[idx].tat / p[idx].bt;
+        p[idx].completed = true;
+        completed++;
+
+        gantt += " | P" + to_string(p[idx].id) + "(" + to_string(p[idx].ct - p[idx].bt) + "-" + to_string(p[idx].ct) + ")";
+    }
+
+    float total_tat = 0, total_wt = 0, total_rt = 0, total_pr = 0;
+    int total_time = 0;
+    for (auto &x : p) {
+        total_tat += x.tat;
+        total_wt += x.wt;
+        total_rt += x.rt;
+        total_pr += x.pr;
+        total_time = max(total_time, x.ct);
+    }
+
+    cout << "\nPROCESS\tAT\tBT\tCT\tTAT\tWT\tRT\tPenalty\n";
+    for (auto &x : p) {
+        cout << "P" << x.id << "\t" << x.at << "\t" << x.bt << "\t" << x.ct
+             << "\t" << x.tat << "\t" << x.wt << "\t" << x.rt << "\t"
+             << fixed << setprecision(2) << x.pr << "\n";
+    }
+
+    cout << fixed << setprecision(2);
+    cout << "\nAverage Waiting Time: " << total_wt / n;
+    cout << "\nAverage Turnaround Time: " << total_tat / n;
+    cout << "\nAverage Response Time: " << total_rt / n;
+    cout << "\nAverage Penalty Ratio: " << total_pr / n;
+    cout << "\nThroughput: " << (float)n / total_time << "\n";
+
+    cout << "\nGantt Chart:\n" << gantt << " |\n";
+}
