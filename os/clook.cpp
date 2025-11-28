a19a5f3277dac8d7546b7690e7c9a2bb8384adca
Tushar Sonar <107312969+the-tushar-sonar@users.noreply.github.com>
2025-11-28 12:22:50 +0530

    Implementation of Disk-scheduling algorithms C-LOOK

---
+++ os/clook.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+void clook(vector<int> requests, int head)
+{
+    sort(requests.begin(), requests.end());
+
+    vector<int> left, right;
+    for (int r : requests)
+    {
+        if (r < head)
+            left.push_back(r);
+        else
+            right.push_back(r);
+    }
+
+    int total = 0;
+    int current = head;
+
+    cout << "\nC-LOOK order: ";
+
+    // Move right
+    for (int r : right)
+    {
+        total += abs(current - r);
+        current = r;
+        cout << r << " ";
+    }
+
+    // Jump to smallest left request
+    if (!left.empty())
+    {
+        total += abs(current - left[0]);
+        current = left[0];
+    }
+
+    // Move right again (through left side)
+    for (int r : left)
+    {
+        total += abs(current - r);
+        current = r;
+        cout << r << " ";
+    }
+
+    cout << "\nTotal Head Movement: " << total << endl;
+}
+
+int main()
+{
+    vector<int> requests = {82, 170, 43, 140, 24, 16, 190};
+    int head = 50;
+
+    clook(requests, head);
+
+    return 0;
+}
