8be85715e209d666a85f6f817e797613ce1d7df0
Tushar Sonar <mylordtushar@gmail.com>
2025-12-17 10:42:14 +0000

    Refactor parseCommand function and improve command handling in mini Linux shell

--- os/lab11/cli.cpp
+++ os/lab11/cli.cpp
@@ -3,12 +3,10 @@
 #include <sys/wait.h>
 using namespace std;
 
-vector<char *> parseCommand(string input)
-{
-    vector<char *> args;
+vector<char*> parseCommand(string &input) {
+    vector<char*> args;
     char *token = strtok(&input[0], " ");
-    while (token != nullptr)
-    {
+    while (token != nullptr) {
         args.push_back(token);
         token = strtok(nullptr, " ");
     }
@@ -16,44 +14,48 @@ vector<char *> parseCommand(string input)
     return args;
 }
 
-int main()
-{
+int main() {
     string input;
 
-    while (true)
-    {
+    while (true) {
         char cwd[1024];
         getcwd(cwd, sizeof(cwd));
         cout << "mini-linux:" << cwd << "$ ";
 
         getline(cin, input);
+        if (input.empty()) continue;
 
-        if (input.empty())
-            continue;
+        vector<char*> args = parseCommand(input);
 
-        if (input == "exit")
+        /* ---- BUILT-IN COMMANDS ---- */
+
+        if (strcmp(args[0], "exit") == 0)
             break;
 
-        if (input == "clear")
-        {
+        if (strcmp(args[0], "clear") == 0) {
             system("clear");
             continue;
         }
 
-        vector<char *> args = parseCommand(input);
+        /* -------- FIXED CD COMMAND -------- */
+        if (strcmp(args[0], "cd") == 0) {
+            if (args[1] == nullptr) {
+                cerr << "cd: missing argument\n";
+            } else {
+                if (chdir(args[1]) != 0)
+                    perror("cd");
+            }
+            continue;
+        }
 
+        /* ---- EXTERNAL COMMANDS ---- */
         pid_t pid = fork();
 
-        if (pid == 0)
-        {
+        if (pid == 0) {
             if (execvp(args[0], args.data()) < 0)
-            {
                 perror("Command not found");
-            }
             exit(0);
-        }
-        else
-        {
+        } else {
             wait(nullptr);
         }
     }
